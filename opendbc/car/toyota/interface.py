6d4b17112370bf4215a5e270fd1687359e9faff8
CT921 <tim.chen242@gmail.com>
2024-11-26 12:30:49 +0800

    update

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -156,8 +156,8 @@ class CarInterface(CarInterfaceBase):
 
     tune = ret.longitudinalTuning
 
-    tune.kiBP = [0.,  1.,   2.,    5.,     8.,    12.,   20.,  27.]
-    tune.kiV = [0.34, 0.33, 0.295, 0.2409, 0.225, 0.205, 0.17, 0.10]
+    tune.kiBP = [0.,   2.,    3.,    5.,     8.,    12.,   20.,  27.]
+    tune.kiV = [0.327, 0.297, 0.276, 0.2405, 0.225, 0.205, 0.17, 0.10]
 
     if candidate in TSS2_CAR:
       ret.stopAccel = -0.4
