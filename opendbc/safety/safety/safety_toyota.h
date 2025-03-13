10ac64e36bb15516e346cc77bdea22974949f488
CT921 <tim.chen242@gmail.com>
2025-03-13 21:00:10 +0800

    update
    This reverts commit 5ba947a757039675f288fb113c448924cc02b0c9.

--- opendbc/safety/safety/safety_toyota.h
+++ opendbc/safety/safety/safety_toyota.h
@@ -461,6 +461,8 @@ static bool toyota_fwd_hook(int bus_num, int addr) {
     is_lkas_msg |= toyota_secoc && (addr == 0x131);
     // in TSS2 the camera does ACC as well, so filter 0x343
     bool is_acc_msg = (addr == 0x343);
+    // Block AEB when stoped to use as a automatic brakehold
+    bool is_aeb_msg = ((addr == 0x344) && (alternative_experience & ALT_EXP_ALLOW_AEB));
     block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal) || (is_aeb_msg && !vehicle_moving && acc_main_on && !gas_pressed);
   }
 
